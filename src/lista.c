a8daca04d26656aa69772fc55647afca8979ee80
rbruno22 <rbruno@libgot.com>
2023-10-02 11:32:04 -0300

    test

--- src/lista.c
+++ src/lista.c
@@ -87,8 +87,8 @@ lista_t *lista_insertar_en_posicion(lista_t *lista, void *elemento,
   if (!nuevo_nodo)
     return NULL;
 
-  // if (posicion == 0 || !lista->nodo_inicio) {
-  if (posicion == 0) {
+  if (posicion == 0 || !lista->nodo_inicio) {
+  // if (posicion == 0) {
     nuevo_nodo->siguiente = lista->nodo_inicio;
     lista->nodo_inicio = nuevo_nodo;
     // if (!lista->nodo_fin || posicion == 0) {
