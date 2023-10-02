52c69f2019c588b79d5b3338919c672a07136477
rbruno22 <rbruno@libgot.com>
2023-10-02 10:35:25 -0300

    Add clang-format

--- src/cola.c
+++ src/cola.c
@@ -2,76 +2,69 @@
 #include "lista.h"
 
 struct _cola_t {
-	lista_t *lista;
+  lista_t *lista;
 };
 
-cola_t *cola_crear()
-{
-	cola_t *cola = malloc(sizeof(cola_t));
-	if (!cola) {
-		return NULL;
-	}
+cola_t *cola_crear() {
+  cola_t *cola = malloc(sizeof(cola_t));
+  if (!cola) {
+    return NULL;
+  }
 
-	cola->lista = lista_crear();
-	if (!cola->lista) {
-		free(cola);
-		return NULL;
-	}
-	return cola;
+  cola->lista = lista_crear();
+  if (!cola->lista) {
+    free(cola);
+    return NULL;
+  }
+  return cola;
 }
 
-cola_t *cola_encolar(cola_t *cola, void *elemento)
-{
-	if (!cola) {
-		return false;
-	}
+cola_t *cola_encolar(cola_t *cola, void *elemento) {
+  if (!cola) {
+    return false;
+  }
 
-	if (!lista_insertar(cola->lista, elemento)) {
-		return NULL;
-	}
-	return cola;
+  if (!lista_insertar(cola->lista, elemento)) {
+    return NULL;
+  }
+  return cola;
 }
 
-void *cola_desencolar(cola_t *cola)
-{
-	if (!cola || lista_vacia(cola->lista)) {
-		return NULL;
-	}
+void *cola_desencolar(cola_t *cola) {
+  if (!cola || lista_vacia(cola->lista)) {
+    return NULL;
+  }
 
-	return lista_quitar_de_posicion(cola->lista, 0);
+  return lista_quitar_de_posicion(cola->lista, 0);
 }
 
-void *cola_frente(cola_t *cola)
-{
-	if (!cola || lista_vacia(cola->lista)) {
-		return NULL;
-	}
+void *cola_frente(cola_t *cola) {
+  if (!cola || lista_vacia(cola->lista)) {
+    return NULL;
+  }
 
-	return lista_elemento_en_posicion(cola->lista, 0);
+  return lista_elemento_en_posicion(cola->lista, 0);
 }
 
-size_t cola_tamanio(cola_t *cola)
-{
-	if (!cola) {
-		return 0;
-	}
-	return lista_tamanio(cola->lista);
+size_t cola_tamanio(cola_t *cola) {
+  if (!cola) {
+    return 0;
+  }
+  return lista_tamanio(cola->lista);
 }
 
-bool cola_vacia(cola_t *cola)
-{
-	if (!cola) {
-		return true;
-	}
-	return lista_vacia(cola->lista);
+bool cola_vacia(cola_t *cola) {
+  if (!cola) {
+    return true;
+  }
+  return lista_vacia(cola->lista);
 }
 
-void cola_destruir(cola_t *cola)
-{
-	if (!cola) {
-		return;
-	}
+void cola_destruir(cola_t *cola) {
+  if (!cola) {
+    return;
+  }
 
-	lista_destruir(cola->lista);
-	free(cola);
+  lista_destruir(cola->lista);
+  free(cola);
 }
